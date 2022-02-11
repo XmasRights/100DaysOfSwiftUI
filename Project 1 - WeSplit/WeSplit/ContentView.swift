87c9a74e0683280bb9e94661de75baf1912fc913
Christopher Fonseka <chris.fonseka@googlemail.com>
2022-02-11 11:12:28 +0000

    WeSplit implemented

--- Project 1 - WeSplit/WeSplit/ContentView.swift
+++ Project 1 - WeSplit/WeSplit/ContentView.swift
@@ -8,59 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var peoplePickerSelection = 2
+    @State private var tipPercentage = 20
 
-    let students = ["Harry", "Ron", "Hermione"]
-    @State var selected = "Harry"
+    let tipPercentages = [10, 15, 20, 25, 0]
+
+    var numberOfPeople: Double {
+        Double(peoplePickerSelection) + 2.0
+    }
+
+    var selectedTip: Double {
+        Double(tipPercentage)
+    }
+
+    var totalPerPerson: Double {
+        let tipValue = checkAmount / 100 * selectedTip
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / numberOfPeople
+
+        return amountPerPerson
+    }
 
     var body: some View {
         NavigationView {
             Form {
-                Section("Inline") {
-                    Picker("Select a Student", selection: $selected) {
-                        ForEach(students, id: \.self) {
-                            Text("\($0)")
+                Section {
+                    TextField(
+                        "Amount",
+                        value: $checkAmount,
+                        format: .currency(
+                            code: Locale.current.currencyCode ?? "GBP"
+                        )
+                    )
+                        .keyboardType(.decimalPad)
+
+                    Picker("Number of People", selection: $peoplePickerSelection) {
+                        ForEach(2..<100) {
+                            Text("\($0) People")
                         }
                     }
-                    .pickerStyle(.inline)
                 }
 
-                Section("Automatic") {
-                    Picker("Select a Student", selection: $selected) {
-                        ForEach(students, id: \.self) {
-                            Text("\($0)")
-                        }
-                    }
-                    .pickerStyle(.automatic)
-                }
-
-                Section("Menu") {
-                    Picker("Select a Student", selection: $selected) {
-                        ForEach(students, id: \.self) {
-                            Text("\($0)")
-                        }
-                    }
-                    .pickerStyle(.menu)
-                }
-
-                Section("Segmented") {
-                    Picker("Select a Student", selection: $selected) {
-                        ForEach(students, id: \.self) {
-                            Text("\($0)")
+                Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
                         }
                     }
                     .pickerStyle(.segmented)
+                } header: {
+                    Text("What tip would you like to leave?")
                 }
 
-                Section("Wheel") {
-                    Picker("Select a Student", selection: $selected) {
-                        ForEach(students, id: \.self) {
-                            Text("\($0)")
-                        }
-                    }
-                    .pickerStyle(.wheel)
+                Section {
+                    Text(
+                        totalPerPerson,
+                        format: .currency(
+                            code: Locale.current.currencyCode ?? "GBP"
+                        )
+                    )
+                } header: {
+                    Text("You Pay")
                 }
             }
-            .navigationTitle("Picker Examples")
+            .navigationTitle("WeSplit")
         }
     }
 }
