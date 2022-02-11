7e40d1bc884382f731ebc540e87608b9c6f5c187
Christopher Fonseka <chris.fonseka@googlemail.com>
2022-02-11 10:43:43 +0000

    Played around with various picker styles

--- Project 1 - WeSplit/WeSplit/ContentView.swift
+++ Project 1 - WeSplit/WeSplit/ContentView.swift
@@ -8,9 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    let students = ["Harry", "Ron", "Hermione"]
+    @State var selected = "Harry"
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section("Inline") {
+                    Picker("Select a Student", selection: $selected) {
+                        ForEach(students, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.inline)
+                }
+
+                Section("Automatic") {
+                    Picker("Select a Student", selection: $selected) {
+                        ForEach(students, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.automatic)
+                }
+
+                Section("Menu") {
+                    Picker("Select a Student", selection: $selected) {
+                        ForEach(students, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.menu)
+                }
+
+                Section("Segmented") {
+                    Picker("Select a Student", selection: $selected) {
+                        ForEach(students, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+
+                Section("Wheel") {
+                    Picker("Select a Student", selection: $selected) {
+                        ForEach(students, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .pickerStyle(.wheel)
+                }
+            }
+            .navigationTitle("Picker Examples")
+        }
     }
 }
 
