e56c25a130a9692a36f44f47d53b98ac8c2857f8
Christopher Fonseka <chris.fonseka@googlemail.com>
2022-04-23 21:30:19 +0100

    Interesting data shown in Moonshot

--- Project 8 - Moonshot/Moonshot/ContentView.swift
+++ Project 8 - Moonshot/Moonshot/ContentView.swift
@@ -9,11 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
 
-    let astronauts = Bundle.main.decode("astronauts.json")
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+
+    let cols = [
+        GridItem(.adaptive(minimum: 150))
+    ]
 
     var body: some View {
-        Text("\(astronauts.count)")
-            .padding()
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: cols) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("DetaiLView")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Moonshot")
+        }
     }
 }
 
