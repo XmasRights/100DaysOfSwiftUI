75094345398c9c835fcc294fdf13cbe34f7a6cd0
Christopher Fonseka <chris.fonseka@googlemail.com>
2023-05-03 22:57:14 +0100

    Added Project 9 - Drawing

---
+++ Project 9 - Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Christopher Fonseka on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
