6647b9256cf0f38e1427544def966e8cd488e313
Christopher Fonseka <chris.fonseka@googlemail.com>
2023-05-03 23:15:26 +0100

    Everything up to insettable shapes

--- Project 9 - Drawing/Drawing/ContentView.swift
+++ Project 9 - Drawing/Drawing/ContentView.swift
@@ -7,15 +7,50 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: .init(x: rect.midX, y: rect.minY))
+        path.addLine(to: .init(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: .init(x: rect.minX, y: rect.maxY))
+        path.closeSubpath()
+
+        return path
+    }
+}
+
+struct Arc: InsettableShape {
+
+    let startAngle: Angle
+    let endAngle: Angle
+    let clockwise: Bool
+
+    var insetAmount = 0.0
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.addArc(
+            center: .init(x: rect.midX, y: rect.midY),
+            radius: (rect.width / 2.0) - insetAmount,
+            startAngle: startAngle - .degrees(90),
+            endAngle: endAngle - .degrees(90),
+            clockwise: !clockwise
+        )
+        return path
+    }
+
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+            .strokeBorder(.blue, lineWidth: 40)
     }
 }
 
